63cad36ba5395b6c9f04c15715ea7f4003372c0f
Aniket <aniketbotre007@gmail.com>
2025-01-15 12:23:05 +0530

    Implement user login functionality with validation for username and password

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,28 +1,66 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 let books = require("./booksdb.js");
+const { message } = require("prompt");
 const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
+const isValid = (username) => {
+  //returns boolean
+  //write code to check is the username is valid
+  for (let user in users) {
+    if (users[user].username === username) {
+      return true;
+    }
+  }
+  return false;
+};
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+const authenticatedUser = (username, password) => {
+  //returns boolean
+  //write code to check if username and password match the one we have in records.
+  for (let user in users) {
+    if (
+      users[user].username === username &&
+      users[user].password === password
+    ) {
+      return true;
+    }
+  }
+  return false;
+};
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
+regd_users.post("/login", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let newUser = req.body;
+
+  if (!newUser.username || !newUser.password) {
+    return res
+      .status(400)
+      .json({ message: "Please enter your username and password" });
+  } else if (isValid(newUser.username)) {
+    if (authenticatedUser(newUser.username, newUser.password)) {
+      let token = jwt.sign(
+        { username: newUser.username },
+        "fingerprint_customer"
+      );
+      return res
+        .status(200)
+        .json({ message: "Customer succesfully logged in!", token: token });
+    } else {
+      return res.status(400).json({ message: "Invalid username or password" });
+    }
+  } else {
+    return res.status(400).json({ message: "Customer not registered" });
+  }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 module.exports.authenticated = regd_users;
